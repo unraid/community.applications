5cd0b41e0ef223503f4469acfcee0745752e80b7
Andrew Z <unraidsquid@gmail.com>
2025-01-01 10:41:52 -0500

    Prevent h1 tags being  generated at start of overview in sidebar

--- source/community.applications/usr/local/emhttp/plugins/community.applications/skins/Narrow/skin.php
+++ source/community.applications/usr/local/emhttp/plugins/community.applications/skins/Narrow/skin.php
@@ -617,7 +617,12 @@ function getPopupDescriptionSkin($appNumber) {
   $ovr = str_replace(["[","]"],["<",">"],$ovr);
   $ovr = str_replace("\n","<br>",$ovr);
   $ovr = str_replace("    ","&nbsp;&nbsp;&nbsp;&nbsp;",$ovr);
+  $ovr = trim($ovr);
+  while (startsWith($ovr,"#")) {
+    $ovr = first_str_replace($ovr,"#","");
+  }
   $ovr = markdown(strip_tags($ovr,"<br>"));
+ 
   $template['display_ovr'] = $ovr;
 
   if ( $template['Plugin'] ) {
