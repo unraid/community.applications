8f244121bab396e23f5c85bb3fae79b5fae8c00a
Andrew Z <unraidsquid@gmail.com>
2025-01-28 11:57:59 -0500

    Update copy_to_git.sh

--- copy_to_git.sh
+++ copy_to_git.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+rm -rf "/tmp/GitHub/community.applications/source/community.applications/usr/local/emhttp/plugins/community.applications/"
 mkdir -p "/tmp/GitHub/community.applications/source/community.applications/usr/local/emhttp/plugins/community.applications/"
 
 cp /usr/local/emhttp/plugins/community.applications/* /tmp/GitHub/community.applications/source/community.applications/usr/local/emhttp/plugins/community.applications -R -v -p
